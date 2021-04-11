a1d1ae1ea44c68d9d9b707827ca87e900fdf41a1
korneliaklopecka <79922514+korneliaklopecka@users.noreply.github.com>
2021-04-11 19:54:24 +0200

    Create vue.js

---
+++ google/vue.js
@@ -0,0 +1,16 @@
+var myBody = document.getElementsByTagName('body')[0];
+
+const input = document.querySelector('input');
+input.addEventListener('change', updateValue);
+
+function updateValue(e)
+{
+    if(e.target.value != '')
+    {  
+        myBody.classList.toggle('results');
+    }
+    else
+    {
+        myBody.classList.remove('results');
+    }
+}
